e78ba92742f8195556daeb3852cc214cc2b0763c
Michael Vanderloon <jenkins92886@gmail.com>
2019-11-11 13:09:36 -0500

    Created project

---
+++ game.js
@@ -0,0 +1,40 @@
+let reply = prompt("Do you want to play a game?");
+
+if (reply.toLocaleLowerCase() === "yes") {
+  let playerName = prompt("What is your name?");
+  console.log("Welcome to the match");
+  let playerHealth = 40;
+  let grantHealth = 10;
+  let grantDeath = 0;
+
+  while (playerHealth > 0 && grantDeath < 3) {
+      
+    let attack = Math.floor(Math.random() * 2) + 1;
+    grantRemainingHealth = grantHealth - attack;
+    playerRemainingHealth = playerHealth - attack;
+
+    console.log(
+      `Grant has ${grantRemainingHealth} hit points remaining. ${playerName} has ${playerRemainingHealth} hit points remaining.`
+    );
+
+    grantHealth = grantRemainingHealth;
+    playerHealth = playerRemainingHealth;
+
+    if (grantHealth <= 0) {
+      grantDeath++;
+      grantHealth = 10;
+      console.log("Grant has been defeated");
+    } 
+    if (grantDeath === 3) {
+      console.log(`${playerName} wins! Grant has died 3 times.`);
+      break;
+    } else if (playerHealth === 0) {
+      console.log("You have lost");
+      break;
+    }
+  }
+}  else {
+    console.log("Fine be that way!")
+}
+
+
